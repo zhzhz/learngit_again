6849e1a6ed39b638f9076d8e6c9a5536324842c3
zhzhz <932496816@qq.com>
2019-11-27 08:46:42 +0800

    add led1

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <reg52.h>
 
 sbit LED = P2^0;     //位地址声明，注意：sbit必须小写、P大写！
-
+sbit LED1 = P2^1;
 void main()
 {
     LED = 0;        //点亮小灯
+    LED1 = 1;
     while (1);      //程序停止在这里
 }
